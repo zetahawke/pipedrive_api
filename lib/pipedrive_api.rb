9006afdf77055af8cdb9d4f6d563db0c4e9133ea
zetahawke <michel.szinavel@gmail.com>
2017-10-18 15:50:07 -0300

    initial commit

---
+++ lib/pipedrive_api.rb
@@ -0,0 +1,9 @@
+require "pipedrive_api/version"
+
+module PipedriveApi
+  # Your code goes here...
+  API_TOKEN = 'cb5f483546f55bee37ba4c0f0f3e97f995ff9d52'.freeze
+  COMPANY_DOMAIN = 'test-integration-gem'.freeze
+
+  
+end
